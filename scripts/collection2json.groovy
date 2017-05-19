3ee5f2eaf597ba1a1175b50cbc6efba3092d476f
Ioannis K. Moutsatsos <imoutsatsos@msn.com>
2017-05-19 10:17:56 -0400

    add comments cleanup sample code

--- scripts/collection2json.groovy
+++ scripts/collection2json.groovy
@@ -1,9 +1,10 @@
-?/*Example of building json from a collection
+/*
+Example of building json from a collection
 author: IKM
    for: B2GMeetup-MAY-18-2017
 */
+//the collection is a complex map
 def ds=[SERIAL_ID:[2..6], TYPE:['DynamicReferenceParameter'], CLASS:['org.biouno.unochoice.DynamicReferenceParameter']]
+//output collection as json
 def json= new groovy.json.JsonBuilder(ds)
      println(json.toPrettyString())
-
-?
