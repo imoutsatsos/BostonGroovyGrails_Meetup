3a0275f5ab8497bb0d3240b0f20ccee919881ff8
Ioannis K. Moutsatsos <imoutsatsos@msn.com>
2017-05-24 06:56:53 -0400

    Comment proxy commands

--- scripts/setupGroovyProxy.groovy
+++ scripts/setupGroovyProxy.groovy
@@ -1,13 +1,17 @@
 /* Enable Groovy PROXY SETTINGS if required
 Edit and run this so that you setup a proxy for Groovy
 A successsful test prints the html that is served from the http://biouno.org site
+Configure and uncomment lines below to enable proxy
 
-*/ 
 System.getProperties().put("proxySet", "true");
 System.getProperties().put("proxyHost", "yourProxyServer");
 System.getProperties().put("proxyPort", "nnnn");
+*/
 
-/*Now run a snall test*/
+/*
+Now run a small test. 
+If it succeeds you have set your proxy correctly
+*/
 link='http://biouno.org'
 url=new URL(link)
 url.eachLine{
