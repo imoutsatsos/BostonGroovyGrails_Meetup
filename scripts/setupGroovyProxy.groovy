2f77270a9ed71d5813add9200637e74e27240bf8
Ioannis K. Moutsatsos <imoutsatsos@msn.com>
2017-05-24 06:54:01 -0400

    remove unused code

--- scripts/setupGroovyProxy.groovy
+++ scripts/setupGroovyProxy.groovy
@@ -1,4 +1,4 @@
-/* Enable Groovy PROXY SETTINGS 
+/* Enable Groovy PROXY SETTINGS if required
 Edit and run this so that you setup a proxy for Groovy
 A successsful test prints the html that is served from the http://biouno.org site
 
@@ -10,8 +10,6 @@ System.getProperties().put("proxyPort", "nnnn");
 /*Now run a snall test*/
 link='http://biouno.org'
 url=new URL(link)
-def result=[]
-
 url.eachLine{
     println it
 }
